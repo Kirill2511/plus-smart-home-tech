04e676bdbafd969b15f8d662ceb482cf0b145885
Kirill2511 <kirill.kirill2@yandex.ru>
2026-05-25 02:35:13 +0700

    Add Config Server support (#9)
    Co-authored-by: Кирилл Ларионов <k.larionov@isimplelab.com>

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/service/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/service/AggregationStarter.java
@@ -11,8 +11,8 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.aggregator.config.KafkaConfig;
 import ru.yandex.practicum.aggregator.kafka.AvroSerializer;
 import ru.yandex.practicum.aggregator.kafka.SensorEventDeserializer;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
@@ -28,36 +28,22 @@ import java.util.Properties;
 @RequiredArgsConstructor
 public class AggregationStarter {
     private final SnapshotService snapshotService;
-
-    @Value("${kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-    @Value("${kafka.topics.sensors}")
-    private String sensorsTopic;
-
-    @Value("${kafka.topics.snapshots}")
-    private String snapshotsTopic;
-
-    @Value("${kafka.consumer.group-id}")
-    private String groupId;
-
-    @Value("${kafka.consumer.poll-timeout}")
-    private long pollTimeout;
+    private final KafkaConfig kafkaConfig;
 
     public void start() {
         KafkaConsumer<String, SensorEventAvro> consumer = new KafkaConsumer<>(consumerProperties());
         KafkaProducer<String, SensorsSnapshotAvro> producer = new KafkaProducer<>(producerProperties());
 
         try {
-            consumer.subscribe(List.of(sensorsTopic));
-            log.info("Aggregator subscribed to topic {}", sensorsTopic);
+            consumer.subscribe(List.of(kafkaConfig.getTopics().getSensors()));
+            log.info("Aggregator subscribed to topic {}", kafkaConfig.getTopics().getSensors());
 
             while (true) {
-                for (ConsumerRecord<String, SensorEventAvro> record : consumer.poll(Duration.ofMillis(pollTimeout))) {
+                for (ConsumerRecord<String, SensorEventAvro> record : consumer.poll(Duration.ofMillis(kafkaConfig.getConsumer().getPollTimeout()))) {
                     SensorEventAvro event = record.value();
                     Optional<SensorsSnapshotAvro> snapshot = snapshotService.updateState(event);
                     snapshot.ifPresent(value -> producer.send(
-                            new ProducerRecord<>(snapshotsTopic, value.getHubId(), value)
+                            new ProducerRecord<>(kafkaConfig.getTopics().getSnapshots(), value.getHubId(), value)
                     ));
                 }
 
@@ -82,8 +68,8 @@ public class AggregationStarter {
 
     private Properties consumerProperties() {
         Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getBootstrapServers());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConfig.getConsumer().getGroupId());
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class.getName());
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
@@ -93,7 +79,7 @@ public class AggregationStarter {
 
     private Properties producerProperties() {
         Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getBootstrapServers());
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class.getName());
         return properties;
